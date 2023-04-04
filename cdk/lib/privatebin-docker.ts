921b41065c89d055234ebdd4a6fd70e7d6274452
Natasha <67543397+NovemberTang@users.noreply.github.com>
2023-04-04 16:26:15 +0100

    add a certificate, domain, attempt to deal with subnets

--- cdk/lib/privatebin-docker.ts
+++ cdk/lib/privatebin-docker.ts
@@ -1,21 +1,50 @@
-import { join } from "path";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
-import type { App } from "aws-cdk-lib";
-import { ContainerImage, RepositoryImageProps, } from "aws-cdk-lib/aws-ecs";
+import { GuVpc } from "@guardian/cdk/lib/constructs/ec2";
+import { GuCname } from "@guardian/cdk/lib/constructs/dns";
+import { GuCertificate } from "@guardian/cdk/lib/constructs/acm";
+import { App, Duration } from "aws-cdk-lib";
+import { ContainerImage } from "aws-cdk-lib/aws-ecs";
+import{ GuApplicationLoadBalancer} from "@guardian/cdk/lib/constructs/loadbalancing/alb"
+
 import { ApplicationLoadBalancedFargateService, ApplicationLoadBalancedFargateServiceProps, ApplicationLoadBalancedTaskImageOptions } from "aws-cdk-lib/aws-ecs-patterns";
+import { ISubnet, SubnetType } from "aws-cdk-lib/aws-ec2";
 
 export class PrivatebinDocker extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
-    const imageProps: RepositoryImageProps = {
-    }
 
-    const imageOptions: ApplicationLoadBalancedTaskImageOptions = {
+    const app = "privatebin"
+    const domainName = `${app}.code.dev-gutools.co.uk`
+    const vpc = GuVpc.fromIdParameter(this, "vpc")
+    const subnets: ISubnet[] = GuVpc.subnetsFromParameter(this)
+
+    const certificate = new GuCertificate(this, {app,
+      domainName,})
+
+    const taskImageOptions: ApplicationLoadBalancedTaskImageOptions = {
       image: ContainerImage.fromRegistry("privatebin/nginx-fpm-alpine:1.5.1")
     }
-    const fargateProps: ApplicationLoadBalancedFargateServiceProps = {publicLoadBalancer: true}
-    const pb = new ApplicationLoadBalancedFargateService(this, "privatebin")
+    const fargateProps: ApplicationLoadBalancedFargateServiceProps = {
+      //publicLoadBalancer: true,
+      taskImageOptions,
+      vpc,
+      taskSubnets: {
+        subnetType: SubnetType.PRIVATE_ISOLATED,
+        subnets },
+      certificate,
+      loadBalancer: new GuApplicationLoadBalancer(this, `${app}-alb`, {app, vpc, internetFacing: false,})
+    }
+    const pb = new ApplicationLoadBalancedFargateService(this, `${app}-service`, fargateProps)
+
+    const cnameProps = {
+      app,
+      domainName,
+      ttl: Duration.hours(1),
+      resourceRecord: pb.loadBalancer.loadBalancerDnsName}
+    new GuCname(this, `${app}-cname`, cnameProps)
+
+    pb.loadBalancer.loadBalancerDnsName
   }
 }
 
